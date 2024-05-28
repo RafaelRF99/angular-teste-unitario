b4c0db67b4b88d52ecd4c101d5db6d8ca4d84d1f
Rafael Ferreira <rafaelfferreira99@hotmail.com>
2024-05-28 09:26:21 -0300

    feat: teste unitario

--- src/app/pages/formulario/formulario.component.spec.ts
+++ src/app/pages/formulario/formulario.component.spec.ts
@@ -1,6 +1,11 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { FormularioComponent } from './formulario.component';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { ReactiveFormsModule } from '@angular/forms';
+import { MatInputModule } from '@angular/material/input';
+import { NoopAnimationsModule } from '@angular/platform-browser/animations';
+import { By } from '@angular/platform-browser';
 
 describe('FormularioComponent', () => {
   let component: FormularioComponent;
@@ -8,7 +13,13 @@ describe('FormularioComponent', () => {
 
   beforeEach(() => {
     TestBed.configureTestingModule({
-      declarations: [FormularioComponent]
+      declarations: [FormularioComponent],
+      imports: [
+        MatFormFieldModule,
+        ReactiveFormsModule,
+        MatInputModule,
+        NoopAnimationsModule,
+      ],
     });
     fixture = TestBed.createComponent(FormularioComponent);
     component = fixture.componentInstance;
@@ -17,5 +28,60 @@ describe('FormularioComponent', () => {
 
   it('should create', () => {
     expect(component).toBeTruthy();
+  });
+
+  it('Formulario deve ser invalido', () => {
+    const result = component.form.valid;
+
+    expect(result).toBeFalse();
+  });
+
+  it('Formulario deve ser valido', () => {
+    component.form.controls['nome'].setValue('Rafael');
+    component.form.controls['idade'].setValue('12');
+
+    const result = component.form.valid;
+
+    expect(result).toBeTrue();
+  });
+
+  it('Botão desativado caso formulario invalido', () => {
+    const button = fixture.debugElement.query(By.css('button')).nativeElement;
+
+    component.form.controls['nome'].setValue('');
+    component.form.controls['idade'].setValue('');
+    fixture.detectChanges();
+
+    expect(button.disabled).toBeTrue();
+  });
+
+  it('Botão ativado caso formulario valido', () => {
+    const button = fixture.debugElement.query(By.css('button')).nativeElement;
+
+    component.form.controls['nome'].setValue('Rafael');
+    component.form.controls['idade'].setValue('25');
+    fixture.detectChanges();
+
+    expect(button.disabled).toBeFalse();
+  });
+
+  it('Campo erro deve exibir caso informações erradas', () => {
+    component.form.controls['nome'].markAsTouched();
+    component.form.controls['idade'].markAsTouched();
+    fixture.detectChanges();
+
+    const nomeError = fixture.debugElement.query(
+      By.css('mat-form-field:nth-child(1) mat-error')
+    );
+    const idadeError = fixture.debugElement.query(
+      By.css('mat-form-field:nth-child(2) mat-error')
+    );
+
+    expect(nomeError.nativeElement.textContent.trim()).toBe(
+      '*Campo obrigatório'
+    );
+    expect(idadeError.nativeElement.textContent.trim()).toBe(
+      '*Campo obrigatório'
+    );
   });
 });
